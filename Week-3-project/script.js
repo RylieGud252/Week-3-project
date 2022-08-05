fc1276963dbb3a82d7ac01ab30654f37fc9efaa6
RylieGud252 <108438452+RylieGud252@users.noreply.github.com>
2022-08-04 22:49:41 -0400

    Add files via upload

---
+++ Week-3-project/script.js
@@ -0,0 +1,25 @@
+// Assignment code here
+function() {
+var generateBtn = cancelIdleCallback
+generateBtn.addEventListener('click')
+ generatePassword
+ var password = generatePassword('click')
+ var x = document.getElementById("myPsw")
+ var x = documert.createElement("input")
+ InputEvent.addEventListener("click")
+ value="8 to 128 characters"
+}
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
